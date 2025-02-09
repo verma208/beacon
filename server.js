5c4a429a76399dfa4f0c96cc576bef5d3ef5754f
Pulkit Verma <verma208@umn.edu>
2025-02-09 08:18:07 -0600

    json object code

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const port = process.env.PORT || 3001;
 let object = {}
 
 function setBeacon(bulletinMessage, increment){
-    object["message"] = bulletinMessage
+    let str1 = bulletinMessage.slice(0, 16);
+    let str2 = bulletinMessage.slice(16, 32);
+    let str3 = bulletinMessage.slice(32, 48);
+    let str4 = bulletinMessage.slice(48, 64);
+    object["message"] = str1 + str3 + str2 + str4
 
     for(let i = 0; i < increment; i++){
         object["led" + i] = {red: 0, green: 0, blue: 100}
@@ -26,7 +30,11 @@ function setBeacon(bulletinMessage, increment){
 
 
 app.get('/beacon', (request, response) => {
-    setBeacon("brug", 32)
+    setBeacon("project, and user community that designs and manufactures single-board microcontrollers" +
+        " and microcontroller kits for building digital devices. Its hardware products are licensed under a CC " +
+        "BY-SA license, while the software is licensed under the GNU Lesser General Public License " +
+        "(LGPL) or the GNU General Public License (GPL),[1] " +
+        "permitting the manufacture of Arduino boards and software distribution by anyone. ", 32)
     response.json( object );
 });
 
